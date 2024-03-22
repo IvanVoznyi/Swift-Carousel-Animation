abdb490001cfc9ff0b26cba096732597ba6cbaef
Ivan Voznyi <ivan.voznyi92@gmail.com>
2024-03-22 20:13:40 +0200

    Initial Commit

---
+++ Swipe Carousel Animation/Swipe_Carousel_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swipe_Carousel_AnimationApp.swift
+//  Swipe Carousel Animation
+//
+//  Created by Ivan Voznyi on 22.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Swipe_Carousel_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
